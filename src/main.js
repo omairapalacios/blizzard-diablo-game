de07aaba50646e7fad737dc1e313f64f38187207
omairapalacios <cpalaciosanchez@hotmail.com>
2021-04-05 13:40:38 -0500

    add layout, header and footer components

--- src/main.js
+++ src/main.js
@@ -1,30 +1,54 @@
-import Vue
-  from 'vue'
-import App
-  from './App.vue'
-import router
-  from './router'
-import store
-  from './store'
-import BootstrapVue
-  from 'bootstrap-vue'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
+import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// CSS global
 import './assets/css/main.styl'
+
+// Le añadimos los iconos que acabamos de importar (todos, los de tipo solid y de tipo brand)
+library.add(
+  faSkull,
+  faCrown,
+  faDungeon,
+  faHatWizard,
+  faHammer,
+  faGem,
+  faVuejs,
+  faBootstrap,
+  faFontAwesome,
+  faGithub,
+  faBattleNet
+)
+
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
-new Vue(
-  {
-    router,
-    store,
-    created () {
-      this.init()
-    },
-    methods: {
-      init () {
-        store.dispatch('oauth/getToken', null, { root: true })
-      }
-    },
-    render: h => h(App)
-  }
-).$mount('#app')
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+new Vue({
+  router,
+  store,
+  methods: {
+    // Nuestra función
+    init () {
+      console.log('Hola 🌝')
+      store.dispatch('oauth/getToken')
+    }
+  },
+  // Hook created
+  created () {
+    this.init()
+  },
+  render: h => h(App)
+}).$mount('#app')
