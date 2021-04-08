18738dc362f63b894e4b16a94d3b72813e851352
omairapalacios <cpalaciosanchez@hotmail.com>
2021-04-08 01:16:41 -0500

    change styles and add component to show heroes top

--- src/components/FooterBar/Index.vue
+++ src/components/FooterBar/Index.vue
@@ -43,7 +43,7 @@ export default {
         {
           classes: ['fab', 'github'],
           color: '#e4e4e4',
-          href: 'https://github.com/baumannzone'
+          href: 'https://github.com/omairapalacios'
         },
         {
           classes: ['fab', 'font-awesome'],
